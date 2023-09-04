1863e007e70f350e2ddbcc19fbcc6af405fb8726
kadiirhocaoglu <kadiirhocaoglu@hotmail.com>
2023-09-04 23:11:57 +0300

    Complete ft

--- LinkedinPostReactAnimation/ViewController.swift
+++ LinkedinPostReactAnimation/ViewController.swift
@@ -15,10 +15,42 @@ class ViewController: UIViewController {
     }()
     
     let iconContainerView: UIView = {
+        //MARK: - Elements
         let containerView = UIView()
-        containerView.backgroundColor = .red
-        containerView.frame = CGRect(x: 0 , y: 0, width: 120, height: 40)
+      
+        //MARK: - Constants
+        let imageViewHeight: CGFloat = 35
+        let imageViewWidth: CGFloat = 35
+        let padding: CGFloat =  8
+        let images = ["Linkedin-Like", "Linkedin-Celebrate", "Linkedin-Support", "Linkedin-Love", "Linkedin-Insightful", "linkedin-funny"]
         
+        let arrangedSubviews = images.map({ (imageName) -> UIImageView in
+            let i = UIImageView(image: UIImage(named: imageName))
+            i.layer.cornerRadius = imageViewHeight / 2
+            i.isUserInteractionEnabled = true
+            return i
+        })
+        let numIcons = CGFloat(arrangedSubviews.count)
+        let containerViewWitdh: CGFloat = numIcons * imageViewWidth + (numIcons + 1) * padding
+        let containerViewHeight: CGFloat = imageViewHeight + 2 * padding
+        
+        //MARK: - Properties
+        let stackView = UIStackView(arrangedSubviews: arrangedSubviews)
+        containerView.backgroundColor = .white
+        stackView.distribution = .fillEqually
+        stackView.spacing = padding
+        stackView.layoutMargins = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        stackView.isLayoutMarginsRelativeArrangement = true
+        containerView.addSubview(stackView)
+        containerView.frame = CGRect(x: 0 , y: 0, width: containerViewWitdh, height: containerViewHeight)
+        containerView.layer.cornerRadius = containerViewHeight / 5
+        containerView.layer.shadowColor = UIColor(white: 0.4, alpha: 0.4).cgColor
+        containerView.layer.shadowRadius = 8
+        containerView.layer.shadowOpacity = 0.5
+        containerView.layer.shadowOffset = CGSize(width: 0, height: 4)
+        stackView.frame = containerView.frame
+        
+        //MARK: - Return
         return containerView
     }()
     // MARK: - Life Cycle
@@ -40,24 +72,56 @@ class ViewController: UIViewController {
         view.addGestureRecognizer(UILongPressGestureRecognizer(target: self, action: #selector(handLongpress)))
     }
     @objc func handLongpress(gesture: UILongPressGestureRecognizer) {
-        print("Long Press", Date())
         if gesture.state == .began {
-            view.addSubview(iconContainerView)
-            let pressedLocation = gesture.location(in: self.view)
-            iconConteinerAlphaAnimated(pressedLocation: pressedLocation)
+            handleGestureBegan(gesture: gesture)
         }else if gesture.state == .ended {
-            iconContainerView.removeFromSuperview()
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut ,animations: {
+                let stackView = self.iconContainerView.subviews.first
+                stackView?.subviews.forEach({ (imageView) in
+                    imageView.transform = .identity
+                })
+                self.iconContainerView.transform = self.iconContainerView.transform.translatedBy(x: 0, y: 50)
+                self.iconContainerView.alpha = 0
+            }, completion: {(_) in
+                self.iconContainerView.removeFromSuperview()
+            })
+        
+                            
+        }else if gesture.state == .changed{
+            handleGestureChanged(gesture: gesture)
         }
     }
-    func iconConteinerAlphaAnimated(pressedLocation: CGPoint) {
+
+    fileprivate func handleGestureChanged(gesture: UILongPressGestureRecognizer){
+        let pressedLocation = gesture.location(in: self.iconContainerView)
+        
+        let hitTestView = iconContainerView.hitTest(pressedLocation, with: nil)
+        
+        if hitTestView is UIImageView {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut ,animations: {
+                let stackView = self.iconContainerView.subviews.first
+                stackView?.subviews.forEach({ (imageView) in
+                    imageView.transform = .identity
+                })
+                hitTestView?.transform = CGAffineTransform(translationX: 0, y: -50)
+            })
+        }
+    }
+    
+    fileprivate func handleGestureBegan(gesture: UILongPressGestureRecognizer){
+        view.addSubview(iconContainerView)
+        let pressedLocation = gesture.location(in: self.view)
+        iconConteinerAlphaAnimated(pressedLocation: pressedLocation)
+    }
+    fileprivate func iconConteinerAlphaAnimated(pressedLocation: CGPoint) {
+        let centeredX = (self.view.frame.width - self.iconContainerView.frame.width)/2
+        iconContainerView.transform = CGAffineTransform(translationX: centeredX, y: pressedLocation.y)
         iconContainerView.alpha = 0
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-            let centeredX = (self.view.frame.width - self.iconContainerView.frame.width)/2
             self.iconContainerView.transform = CGAffineTransform(translationX: centeredX, y: pressedLocation.y - self.iconContainerView.frame.height)
             self.iconContainerView.alpha = 1
         })
     }
-    
     // MARK: - Actions
 
 }
